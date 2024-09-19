dd8dbcacac9cc874a270327733933b076bdf0e83
Gabriel <gabriel.couto2704@gmail.com>
2024-09-19 11:07:47 -0300

    declarando os tipos de dados, var / let / const

---
+++ app.js
@@ -0,0 +1,12 @@
+// alert("Olá mundo")
+
+// Declarando as variaveis
+
+var nome='fiap';
+console.log(nome);
+
+let nome1='cidade';
+console.log(nome1);
+
+const nome2='javascript';
+console.log(nome2);
