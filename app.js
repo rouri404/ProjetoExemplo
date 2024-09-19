21ee39de8683331a286b9703e591a60c365a3b56
Gabriel <gabriel.couto2704@gmail.com>
2024-09-19 11:36:43 -0300

    convertendo variaveis

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 // alert("Olá mundo")
 
 // Declarando as variaveis
@@ -42,3 +43,20 @@ console.log(valor);
 console.log(texto);
 console.log(aluno);
 console.log(tecnologia);
+*/
+
+// Conversões 
+let numerofloat=123.456;
+console.log(parseInt(numerofloat))
+
+// Conversão str para float
+let numerostring = "456.311"
+console.log(parseFloat(numerostring))
+
+// Conversão int para str
+let numeroint = 312.192
+console.log(numeroint.toString())
+
+// Ver o tipo da variavel
+let verTipo = typeof numerofloat
+console.log(verTipo)
