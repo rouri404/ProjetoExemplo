8586630e047f2c8d082aa010a5a43ae66790fbee
Gabriel <gabriel.couto2704@gmail.com>
2024-09-19 11:14:44 -0300

    criando as variáveis indefinidas e vazias

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ console.log(nome1);
 
 const nome2='javascript';
 console.log(nome2);
+
+// Outras declarações
+
+// Declaração indefinida
+
+let nome3;
+console.log(nome3)
+
+// Declaração vazia
+
+let disciplina=null;
+console.log(disciplina)
