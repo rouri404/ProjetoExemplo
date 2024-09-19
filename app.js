0cdd2b945123b4f38e63568c08c7fd711a461768
Gabriel <gabriel.couto2704@gmail.com>
2024-09-19 11:23:08 -0300

    criando tipos de variaves

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // alert("Olá mundo")
 
 // Declarando as variaveis
-
 var nome='fiap';
 console.log(nome);
 
@@ -14,11 +13,32 @@ console.log(nome2);
 // Outras declarações
 
 // Declaração indefinida
-
 let nome3;
 console.log(nome3)
 
 // Declaração vazia
-
 let disciplina=null;
-console.log(disciplina)
+console.log(disciplina);
+
+// Tipos de variaveis
+
+// Inteiro
+let idade = 18;
+
+// Flutuantes ou com casas decimais
+let valor = 6.9;
+
+// Strings (textos)
+let texto="Fiap1";
+
+//Booleanos - Verdadeiro ou False
+let aluno=true
+
+// Arrauy - Conjunto de informações
+let tecnologia = ['Java','C#','HTML','CSS'];
+
+console.log(idade);
+console.log(valor);
+console.log(texto);
+console.log(aluno);
+console.log(tecnologia);
